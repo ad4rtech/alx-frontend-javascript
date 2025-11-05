7ada325d0bad615209471b8c043f41c4b55d3f1d
Fidel Adar <adarfidel.ogw@gmail.com>
2025-11-05 15:54:39 +0300

    4.Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,7 +24,7 @@ interface StudentClassInterface {
     displayName(): string;
 }
 
-class StudentClass implements StudentClassInterface {
+class StudentClass {
     firstName: string;
     lastName: string;
 
