04364a012dec911cefd95ab313493ea3f580844e
Fidel Adar <adarfidel.ogw@gmail.com>
2025-11-05 18:38:58 +0300

    8. Ambient Namespaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,14 +1,16 @@
-import { RowID, RowElement } from "./interface";
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
 import * as CRUD from './crud';
 
 const row: RowElement = {
-    firstName: 'Guillaume',
-    lastName: 'Salva',
+  firstName: 'Guillaume',
+  lastName: 'Salva',
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
 
-const updatedRow: RowElement = { ...row, age:23 };
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
